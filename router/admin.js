5b6432b18683ae52597960c32d2a9939091543c2
liujiahui3 <1609307967@qq.com>
2020-03-26 22:57:45 +0800

    doc

--- router/admin.js
+++ router/admin.js
@@ -2,9 +2,23 @@ const express=require('express')
 const router = express.Router()
 const {findAllAdmin} = require('../dbConctor/admin')
 
+/**
+ * @api {git} /admin/getalladmin   获取管理员信息
+ * @apiName getalladmin
+ * @apiGroup admin
+ *
+ *
+ * @apiSuccess {String} err  状态码.
+ * @apiSuccess {String} msg  信息提示.
+ * @apiSuccess {String} data 管理员信息
+ */
 router.get('/getalladmin',(req,res)=>{
   findAllAdmin().then(result=>{
-    res.send(result)
+    if(result.length > 0){
+      res.send({msg:'获取成功',code:0,data})
+    }else{
+      res.send({msg:'获取失败',code:-1})
+    } 
   })
 })
 
