4c3d6e4682ccc18713e9ea28e2c226955e707e86
lmm-97 <410669783@qq.com>
2020-03-27 20:57:19 +0800

    '2'

--- router/root.js
+++ router/root.js
@@ -3,43 +3,40 @@ const express=require('express')
 const router = express.Router()
 const  Root=require ('../db/rootDb')
 
-/**
- * @api {git} /admin/getalladmin   获取管理员信息
- * @apiName getalladmin
- * @apiGroup admin
- *
- *
- * @apiSuccess {String} err  状态码.
- * @apiSuccess {String} msg  信息提示.
- * @apiSuccess {String} data 管理员信息
- */
 
 //  查询
 router.get('/find',(req,res)=>{//后面
-    Root.find(ctx).then(body=>{
-        res.send(body)
-    })
+    Root.find()
+    .then(data=>res.send({code:0,data,msg:'查询成功'}))
+    .catch(err=>res.send({code:-1,err,msg:'查询失败'}))
 })
 // 添加
 router.post('/add',(req,res)=>{//后面
     let{us,ps}=req.body
     Root.insertMany({us,ps})
-    .then(data=>{res.send(data)})
-    .catch(err=>{res.send(err)})
+    .then(data=>res.send({code:0,data,msg:'添加成功'}))
+    .catch(err=>res.send({code:-1,err,msg:'添加失败'}))
 })
-
+// //删除
+router.post('/del',(req,res)=>{
+    let {_id}=req.body
+    Root.deleteOne({_id})
+    .then(data=>res.send({code:0,data,msg:'删除成功'}))
+    .catch(err=>res.send({code:-1,err,msg:'删除失败'}))
+})
 //登陆
 router.post('/login',(req,res)=>{
     let{us,ps}=req.body
     Root.findOne({us,ps})
     .then(data=>{
         if(data!==null){
-            res.send({code:0,data,msg:'查询成功'})
+            res.send({code:0,data,msg:'登陆成功'})
         }else{
-            res.send({code:-1,msg:'查询失败'})
+            res.send({code:-1,msg:'登陆失败'})
         }
     })
 })
+
     
 
 
