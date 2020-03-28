b0075391d6da3cb412e2bff792b8c2cf20ab24e8
liujiahui3 <1609307967@qq.com>
2020-03-28 14:44:46 +0800

    token缺失提示

--- middleware/superAdmin.js
+++ middleware/superAdmin.js
@@ -3,6 +3,7 @@ const root = require('../db/rootDb')
 let superAdmin = (req,res,next)=>{
   let {token} = req.body
   let {us,ps} = verifyToken(token)
+  if(!token){return res.send({mes:'token缺失',code:-997})}
   root.findOne({us,ps}).then(data=>{
     if(data._id == '5e7df387a0d79a3538887dad'){
       next()
