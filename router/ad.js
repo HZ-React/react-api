4898e0b769b8f7b5d8388e8c27a92fac2ea4d741
mading <849793164@qq.com>
2020-04-02 15:03:00 +0800

    md

--- router/ad.js
+++ router/ad.js
@@ -5,7 +5,7 @@ const martin_ad=require('../db/adDb')
 //查询一个广告的信息
 router.post('/adone',(req,res)=>{
     let {_id}=req.body
-    console.log(_id)
+   
     martin_ad.findOne({_id}).then(data=>{
       res.send({mes:'数据查询成功',data})
     }).catch((err)=>{
@@ -31,7 +31,7 @@ router.get('/list',(req,res)=>{
 router.post('/adadd',(req,res)=>{
     let {name,jump,address,type} =req.body
     martin_ad.insertMany({name,jump,address,type}).then((data)=>{
-    //   console.log(data)
+    
     res.send({err:0,msg:'插入成功'})
   }).catch((err)=>{
     res.send({err:-1,msg:'插入失败'})
@@ -45,7 +45,7 @@ router.post('/addel',(req,res)=>{
     martin_ad.deleteOne({_id})
     .then(()=>{res.send({err:0,msg:'删除成功'})})
     .catch((err)=>{
-      console.log(err)
+     
       res.send({err:-1,msg:'删除失败请重试'})}) 
   })
 
@@ -53,20 +53,20 @@ router.post('/addel',(req,res)=>{
 router.post('/adupdate',(req,res)=>{
     let {_id,obj}=req.body
     obj = JSON.parse(obj)
-    // console.log(obj)
+   
     martin_ad.updateOne({_id},obj).then((data)=>{
     res.send({err:0,msg:'更新成功'})
     }).catch((err)=>{
-        // console.log(err)
+        
     res.send({err:-1,msg:'更新失败'})
     })
 })
 //分页查询
 router.post('/adbypage',async (req,res)=>{
     let allad=await martin_ad.find()
-    console.log(allad)
+   
     let allCount=allad.length
-    console.log("数量",allCount)
+   
     let page = req.body.page||1 //查询的第几页数据
     let pageSize = req.body.pageSize ||2 //每页几条数据
     martin_ad.find().skip((Number(page)-1)*pageSize).limit(Number(pageSize)).then((data)=>{
@@ -83,21 +83,20 @@ let  findAdByType = async (type,page,pageSize) =>{
     let allCount=allAd.length
     let result = await martin_ad.find({type}).skip(Number((page-1)*pageSize)).limit(Number(pageSize))
      
-    // let allCount=result.length
-    // console.log(allCount)
+    
     return {result,allCount}
   }
 router.post('/adbytype',(req,res)=>{
     let {type} = req.body 
     let page = req.body.page||1  
     let pageSize = req.body.pageSize||2
-    console.log("类型",type)
+   
     findAdByType(type,page,pageSize)
     .then((data)=>{
      res.send({err:0,msg:'查询成功',list:data})
    })
    .catch((err)=>{
-       console.log(err)
+      
        res.send({err:-1,msg:'查询失败请重试'})})
   
   })
